c91135e1a465899ff2194aa6fad4dcce36384e2f
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-12-08 11:47:14 +0900

    레이아웃을 로그아웃으로 변경

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,9 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
     <>
-      <h2>레이아웃</h2>
+      <h2>로그아웃</h2>
       <Outlet />
     </>
   );
