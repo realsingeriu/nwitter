f836241d85a57c94332798faaaf2738fb6e8ad93
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-12-12 09:47:37 +0900

    파이어 베이스 수정

--- src/firebase.js
+++ src/firebase.js
@@ -5,11 +5,11 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_ACCESS_NWITTER_API_KEY,
-  authDomain: "nwitter-drv98.firebaseapp.com",
-  projectId: "nwitter-drv98",
-  storageBucket: "nwitter-drv98.appspot.com",
-  messagingSenderId: "97502061943",
-  appId: "1:97502061943:web:7cb2ec968760b4e7a3ad72",
+  authDomain: "nwitter-bae.firebaseapp.com",
+  projectId: "nwitter-bae",
+  storageBucket: "nwitter-bae.appspot.com",
+  messagingSenderId: "740194171531",
+  appId: "1:740194171531:web:574f720a3d3c216d91b28e",
 };
 
 // Initialize Firebase
